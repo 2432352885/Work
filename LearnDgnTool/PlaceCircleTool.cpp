180f96af5e35a1e6193c7d43bcc8bdd8728c7ee8
xl-cpp <2432352885@qq.com>
2023-07-05 10:45:21 +0800

    compelete the triangle place tool

--- LearnDgnTool/PlaceCircleTool.cpp
+++ LearnDgnTool/PlaceCircleTool.cpp
@@ -33,7 +33,11 @@ PlaceCircleTool::PlaceCircleTool():DgnPrimitiveTool(0,0)
 
 void PlaceCircleTool::_OnPostInstall()
 {
-		_BeginDynamics();
+		//_BeginDynamics();
+	//启用精准捕捉
+	AccuSnap::GetInstance().EnableSnap(true);
+	
+	__super::_OnPostInstall();
 }
 
 bool PlaceCircleTool::_OnDataButton(DgnButtonEventCR ev)
@@ -69,7 +73,7 @@ void PlaceCircleTool::_OnRestartTool()
 {
 	InstallNewInstance();
 }
-
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
 
 void PlaceTriangleTool::InstallNewInstance()
 {
@@ -77,29 +81,51 @@ void PlaceTriangleTool::InstallNewInstance()
 	newTool->InstallTool();
 }
 
+WCharCP doubleToConst(double value)
+{
+	//多小数
+	//std::stringstream ss;
+	//ss << value;
+	//std::string str = ss.str();
 
-BentleyStatus PlaceTriangleTool::CreateTriangle(MSElement cell, DPoint3d  ptTiaCenter)
+	//两位小数
+	char buf[5];
+	sprintf(buf, "%.2f", value);
+	string str = buf;
+
+	WCharP buffer = new WChar[str.length() + 1];
+	for(size_t i = 0; i < str.length(); ++i)
+	{
+		buffer[i] = static_cast<WChar>(str[i]);
+	}
+	buffer[str.length()] = 0;
+
+	return reinterpret_cast<WCharCP>(buffer);
+	
+}
+
+BentleyStatus PlaceTriangleTool::CreateTriangle(MSElementDescrP groupDP, DPoint3d  ptTiaCenter)
 {
 	EditElementHandle eehTriangle;
+	EditElementHandle eehTxt;
 	DPoint3d center[3];
 	//获取给定模型参考的每米UOR数
-	double radius = mdlModelRef_getUorPerMeter(ACTIVEMODEL);
+	double radius = mdlModelRef_getUorPerMeter(ACTIVEMODEL)/1000;
 	//倒三角形创建
 	DEllipse3d elli = DEllipse3d::FromCenterRadiusXY(ptTiaCenter, radius);
 	//三角形的点
 	center[0] = center[1] = center[2] = ptTiaCenter;
 
 	center[0].x = ptTiaCenter.x; center[0].y = ptTiaCenter.y;
-	center[1].x = ptTiaCenter.x + 2 * radius; center[1].y = ptTiaCenter.y + 2 * radius;
-	center[2].x = ptTiaCenter.x - 2 * radius; center[2].y = ptTiaCenter.y + 2 * radius;
+	center[1].x = ptTiaCenter.x + 15 * radius; center[1].y = ptTiaCenter.y + 15 * radius;
+	center[2].x = ptTiaCenter.x - 15 * radius; center[2].y = ptTiaCenter.y + 15 * radius;
 
 	CurveVectorPtr pCurveVec = CurveVector::Create(CurveVector::BOUNDARY_TYPE_Outer);
 	pCurveVec->Add(ICurvePrimitive::CreateLineString(center, 3));
 	if(SUCCESS != DraftingElementSchema::ToElement(eehTriangle, *pCurveVec, nullptr, ACTIVEMODEL->Is3d(), *ACTIVEMODEL))
 		return ERROR;
 
-	eehTriangle.GetElementDescrP();
-	mdlElmdscr_appendDscr(cellEdP, eehTriangle.ExtractElementDescr());
+	//旋转
 	RotMatrix rMatrix;
 	mdlRMatrix_fromView(&rMatrix, IViewManager::GetManager().GetCurrentViewNumber(), false);
 	mdlRMatrix_invert(&rMatrix, &rMatrix);
@@ -107,74 +133,77 @@ BentleyStatus PlaceTriangleTool::CreateTriangle(MSElement cell, DPoint3d  ptTiaC
 	TransformInfo transformInfo = TransformInfo(transform);
 	eehTriangle.GetDisplayHandler()->ApplyTransform(eehTriangle, transformInfo);
 	
-	//BentleyStatus bstatus;
-	/*
-	DPoint3d startPoint = center[1];
-	DPoint3d endPoint = {0,0,0};
-	endPoint.x = center[1].x - 20000;
-	endPoint.y = center[1].y + 10000;
-	*/
+//文本
 	DgnFilePtr    pActiveDgnFile = ISessionMgr::GetActiveDgnFile();
 
 	DimensionStylePtr pDimStyle = DimensionStyle::GetSettings(*pActiveDgnFile);
 
 	DgnTextStylePtr textStyle = DgnTextStyle::GetSettings(*pActiveDgnFile);
-	
+	//修改文字类型以及大小位置
+	textStyle->SetProperty(TextStyleProperty::TextStyle_ColorFlag, true);
+	UInt32 color = 0;
+	textStyle->SetProperty(TextStyleProperty::TextStyle_Color, color);
+	double Height = 4 * radius;
+	double Width = 4 * radius;
+	textStyle->SetProperty(TextStyleProperty::TextStyle_Height, Height);
+	textStyle->SetProperty(TextStyleProperty::TextStyle_Width, Width);
+
+	WCharCP ptr =  doubleToConst(center[0].y);
+	textStyle->Replace(ptr);
 	EditElementHandle noteElem, leaderElement;
 	TextBlockPropertiesPtr txtBlockProp = TextBlockProperties::Create(*textStyle, *(ACTIVEMODEL->AsDgnModelP()));
 	ParagraphPropertiesPtr paragraphProperties = ParagraphProperties::Create(*textStyle, *(ACTIVEMODEL->AsDgnModelP()));
 	RunPropertiesPtr runProp = RunProperties::Create(*textStyle, *(ACTIVEMODEL->AsDgnModelP()));
 	TextBlockPtr textbock = TextBlock::Create(*txtBlockProp, *paragraphProperties, *runProp, *(ACTIVEMODEL->AsDgnModelP()));
-	textbock->AppendText(L"4.00");
-
-	EditElementHandle eehTxt;
+	double dx = (center[1].x + center[2].x)/2 - 1;
+	double dy = center[1].y + 5 * radius;
+	
+	textbock->AppendText(ptr);
 	TextHandlerBase::CreateElement(eehTxt, NULL, *textbock);
-	AssocPoint assocPt1;
+
+	delete[] reinterpret_cast<WCharCP>(ptr);
+
+	//移动
+	RotMatrix matrix = RotMatrix::FromIdentity();
+	DPoint3d translation = DPoint3d::From(dx, dy, 0);
+	Transform tran = Transform::From(matrix, translation);
+	TransformInfo transform1 (tran);
+	eehTxt.GetHandler().ApplyTransform(eehTxt, transform1);
+	eehTxt.ReplaceInModel(eehTxt.GetElementRef());
+	//eehTxt.AddToModel();
+
+//合并成cell
+	//MSElementDescrP groupDP;
+	//EditElementHandle  eeh;
+	//NormalCellHeaderHandler::CreateOrphanCellElement(eeh, NULL, ACTIVEMODEL->Is3d(), *ACTIVEMODEL);
+	//mdlElement_memcpy(&cell, eeh.GetElementCP());
+	//mdlElmdscr_new(&groupDP, NULL, &cell);
+	eehTriangle.GetElementDescrP();
+	eehTxt.GetElementDescrP();
+	MSElementDescrP elmDP1 = eehTriangle.ExtractElementDescr();
+	mdlElmdscr_appendDscr(groupDP, elmDP1);
+	MSElementDescrP elmDP2 = eehTxt.ExtractElementDescr();
+	mdlElmdscr_appendDscr(groupDP, elmDP2);
+	
+	//ITxnManager::GetManager().GetCurrentTxn().DeleteElement(elmDP1->h.elementRef);
+	//ITxnManager::GetManager().GetCurrentTxn().DeleteElement(elmDP2->h.elementRef);
+	/*if (groupDP->h.firstElem && groupDP->h.firstElem->h.next)
+	{
+		groupDP->h.isValid = false;
+		mdlCell_setOriginAndRange(groupDP);
+		mdlElmdscr_add(groupDP);
+		if (0L != mdlElmdscr_add(groupDP))
+			SelectionSetManager::GetManager().AddElement(groupDP->h.elementRef, groupDP->h.dgnModelRef);
+		//mdlElmdscr_freeAll(&groupDP);
+	}
+	
+
+	/*AssocPoint assocPt1;
 	AssociativePoint::InitKeypoint(assocPt1, 0, 0, 0, 1);
 	AssociativePoint::SetRoot(assocPt1, eehTxt.GetElementId(), 0);
 	TextHandlerBase::SetupOffsetAssociation(eehTxt, eehTriangle, assocPt1);
-	eehTxt.AddToModel();
+	eehTxt.AddToModel();*/
 
-
-/*
-
-	NoteCellHeaderHandler::StdDPointVector  noteLeaderPoints;
-	noteLeaderPoints.push_back(startPoint);
-	noteLeaderPoints.push_back(endPoint);
-	bstatus = NoteCellHeaderHandler::CreateNote(noteElem, leaderElement, *textbock, *pDimStyle, false, *ACTIVEMODEL, noteLeaderPoints);
-	IDimensionEdit* iDimEdit = dynamic_cast<IDimensionEdit*>(&leaderElement.GetHandler());
-	
-	
-
-	EditElementHandle lineEeh;
-	
-	//DPoint3d ptArr[2] = { startPoint,startPoint };
-	//bstatus = LineStringHandler::CreateLineStringElement(lineEeh, NULL, ptArr,2, false, *ACTIVEMODEL);
-	//lineEeh.AddToModel();
-
-	AssocPoint assocPt1;
-	AssociativePoint::InitKeypoint(assocPt1, 1, 2, 0, 1);
-	//通过用一对元素id设置依赖关系的目标或者根元素 完成新关联点的设置
-	//bstatus = AssociativePoint::SetRoot(assocPt1, eehTriangle.GetElementId(), 0, 0);
-	//eehTriangle.AddToModel();
-
-	if (SUCCESS != iDimEdit->SetPoint(leaderElement, nullptr, &assocPt1, 0))
-	{
-		return ERROR;
-	}
- 
-
-	if (iDimEdit->GetNumPoints(leaderElement) > 0)
-	{
-		iDimEdit->DeletePoint(leaderElement, 0);
-		iDimEdit->InsertPoint(leaderElement, &startPoint, nullptr, *pDimStyle, 0);
-	}
-	iDimEdit->SetRotationMatrix(leaderElement, RotMatrix::FromIdentity());
-
-	
-	bstatus = NoteCellHeaderHandler::AddToModel(noteElem, leaderElement, *(ACTIVEMODEL->AsDgnModelP()));
-	//lineEeh.DeleteFromModel();
-*/
 	return SUCCESS;
 
 }
@@ -188,8 +217,8 @@ void PlaceTriangleTool::_OnPostInstall()
 {
 	//启用动态绘制
 	_BeginDynamics();
-	//AccuSnap::GetInstance().EnableSnap(true);
-	//__super::_OnPostInstall();
+	AccuSnap::GetInstance().EnableSnap(true);
+	__super::_OnPostInstall();
 }
 
 //获取鼠标位置 
@@ -200,30 +229,55 @@ void PlaceTriangleTool::_OnDynamicFrame(DgnButtonEventCR ev)
 	mdlCell_create(&cell, NULL, NULL, FALSE);
 	MSElementDescrP cellEdP;
 	mdlElmdscr_new(&cellEdP, NULL, &cell);
-	if (SUCCESS != CreateTriangle(cellEdp, *ev.GetPoint()))
+	if (SUCCESS != CreateTriangle(cellEdP, *ev.GetPoint()))
 	 	return;
-	RedrawElems     redrawElems;
+	
+	//由于DoRedraw这个接口会自动忽略.next，我们通过循环来预览所有元素
+	RedrawElems redrawElems;
 	redrawElems.SetDynamicsViews(IViewManager::GetActiveViewSet(), ev.GetViewport());
 	redrawElems.SetDrawMode(DRAW_MODE_TempDraw);
 	redrawElems.SetDrawPurpose(DrawPurpose::Dynamics);
-	redrawElems.DoRedraw(eeh);
-	
+	MSElementDescrP ChildedP = cellEdP->h.firstElem;
+	//由于DoRedraw这个接口会自动忽略.next，我们通过循环来预览所有元素
+	while (ChildedP != NULL)
+	{
+			redrawElems.DoRedraw(ChildedP);
+			ChildedP = ChildedP->h.next;
+	}
 }
 
 bool PlaceTriangleTool::_OnResetButton(DgnButtonEventCR ev)
 {
-	//_EndDynamics();
-	//_ExitTool();
-	_OnReinitialize();
+	_EndDynamics();
+	_ExitTool();
+	//EditElementHandle eeh;
+	//MSElement cell;
+	//mdlCell_create(&cell, NULL, NULL, FALSE);
+	//MSElementDescrP cellEdP;
+	//mdlElmdscr_new(&cellEdP, NULL, &cell);
+	//EditElementHandle eeh;
+	///if (CreateTriangle(cellEdP, *ev.GetPoint()))
+		//mdlElmdscr_add(cellEdP);
+
+	//_OnReinitialize();
 	return true;
 }
 
 bool PlaceTriangleTool::_OnDataButton(DgnButtonEventCR ev)
 {
-	EditElementHandle eeh;
-    if (SUCCESS != CreateTriangle(eeh, *ev.GetPoint()))
+	
+	//EditElementHandle eeh;
+	MSElement cell;
+	mdlCell_create(&cell, NULL, NULL, FALSE);
+	MSElementDescrP cellEdP;
+	mdlElmdscr_new(&cellEdP, NULL, &cell);
+    if (SUCCESS != CreateTriangle(cellEdP, *ev.GetPoint()))
 		return false;
-	eeh.AddToModel();
+	
+	mdlElmdscr_add(cellEdP);
+	_BeginDynamics();
+	EnableUndoPreviousStep();//使用Ctrl + Z 可以取消
+	
 	_OnReinitialize();
 	return true;
 }
--- LearnDgnTool/PlaceCircleTool.h
+++ LearnDgnTool/PlaceCircleTool.h
@@ -6,7 +6,10 @@
 #include <Mstn\ISessionMgr.h>
 #include <Mstn\MdlApi\mselems.h>
 #include <DgnPlatform\DgnPlatformApi.h>
+#include <DgnPlatform/TextHandlers.h>
 #include <DgnPlatform/DimensionHandler.h>
+#include <Mstn\MdlApi\dlogids.r.h>
+
 
 
 USING_NAMESPACE_BENTLEY_DGNPLATFORM;
@@ -40,9 +43,11 @@ class PlaceTriangleTool : public DgnPrimitiveTool
 public:
 	static void InstallNewInstance();
 
-	BentleyStatus CreateTriangle(EditElementHandleR eehTriangle, DPoint3d ptTiaCenter);
+	BentleyStatus CreateTriangle(MSElementDescrP cell, DPoint3d ptTiaCenter);
 	
 	PlaceTriangleTool();
+
+
 
 
 private:
@@ -56,70 +61,4 @@ private:
 
 	virtual void _OnRestartTool() override;
 
-
-};
-class DimensionProperty : public IDimCreateData
-{
-private:
-	DimensionStylePtr m_dimstyle;
-	DgnTextStylePtr m_textStyle;
-	Symbology m_symbology;
-	LevelId m_levelId;
-	DirectionFormatterPtr m_directionFormatter;
-	RotMatrix m_rot1;
-	RotMatrix m_rot2;
-
-public:
-	DimensionProperty(DimensionStylePtr dimstyle, DgnTextStylePtr textstyle, Symbology symb, LevelId levelId, DirectionFormatterPtr formatter, RotMatrix  rot1, RotMatrix rot2)
-	{
-		m_dimstyle = dimstyle;
-		m_textStyle = textstyle;
-		m_symbology = symb;
-		m_levelId = levelId;
-		m_directionFormatter = formatter;
-		m_rot1 = rot1;
-		m_rot2 = rot2;
-	}
-
-	virtual DimensionStyleCR _GetDimStyle() const
-	{
-		return *m_dimstyle;
-	}
-
-	virtual DgnTextStyleCR   _GetTextStyle()  const
-	{
-		return *m_textStyle;
-	}
-
-	virtual Symbology   _GetSymbology() const
-	{
-		return m_symbology;
-	}
-	virtual LevelId   _GetLevelID() const
-	{
-		return m_levelId;
-	}
-	virtual DirectionFormatterCR   _GetDirFormat()  const
-	{
-		return *m_dirFormatter;
-	}
-	virtual int  _GetViewNumber()  const
-	{
-		return 0;
-	}
-
-	virtual RotMatrixCR  _GetDimRMatrix()  const
-	{
-		return m_rot1;
-	}
-
-	virtual RotMatrixCR  _GetViewRMatrix()  const
-	{
-		return m_rot2;
-	}
-
-	virtual bool   _GetAnnScaleAllowed()  const
-	{
-		return true;
-	}
 };
